2a57a4473b7a74dc04d19baf5fb9ab27fa8635d8
ofixone <ofixmail@yandex.ru>
2019-02-23 19:00:20 +0300

    Стандартные миграции в папку backend

--- config/console.php
+++ config/console.php
@@ -2,6 +2,12 @@
 
 $config = [
     'controllerNamespace' => 'app\backend\console\controllers',
+	'controllerMap' => [
+        'migrate' => [
+            'class' => \yii\console\controllers\MigrateController::class,
+            'migrationPath' => '@backend/migrations'
+        ]
+    ]
 ];
 
 return $config;
